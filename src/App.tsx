14177aa960a57de6ac6cd51ff36674870892bff7
p <philipp.norton@posteo.de>
2025-01-08 18:50:11 +0100

    feat(app.tsx): add environment component for lighting etc

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import './App.css'
 import { Canvas } from '@react-three/fiber'
 import Instrument from './components/Instrument'
 
+function Environment () {
+  // component for lighting, etc.
+  return (
+    <>
+      <ambientLight intensity={1.0} />
+      <directionalLight color='orange' position={[3,0,3]} />
+      <mesh position={[3,0,3]}><boxGeometry/></mesh> {/* <- for debugging */}
+    </>
+  )
+}
+
 function App() {
 
   return (
-    <div id="canvas-container">
+    <div id='canvas-container'>
       {/* Canvas sets up a Scene and Camera and renders the scene every frame */}
       <Canvas>
+        <Environment />
         <Instrument></Instrument>
       </Canvas>
     </div>
