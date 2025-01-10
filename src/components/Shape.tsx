2621a191c1e8959b6f3c09e4e7407ced70d77c38
p <philipp.norton@posteo.de>
2025-01-10 11:45:01 +0100

    chore: comments & linting

--- src/components/Shape.tsx
+++ src/components/Shape.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
-import { Mesh, Vector3, ThreeEvent } from 'three';  // todo: ask where ThreeEvent comes from (how to properly import types?)
+import { Mesh, Vector3, ThreeEvent } from 'three'; // todo: ask where ThreeEvent comes from (how to properly import types?)
 import { useSpring, animated } from '@react-spring/three';
 import { DragControls, Html } from '@react-three/drei';
 import { Module } from '../App';
@@ -9,9 +9,9 @@ interface ShapeProps {
   type: string;
   color: string;
   position: Vector3;
-  modules: Module[];  // state of App() containing all
-  setModules: Function;  // todo: ask how to better define function (linting error: "Prefer explicitly defining any function parameters and return type")
-}
+  modules: Module[]; // state of App() containing all
+  setModules: Function; // todo: ask how to better define function (linting error: "Prefer explicitly defining any function parameters and return type")
+} // todo: move to type definition file
 
 function Shape(props: ShapeProps) {
   // This reference will give us direct access to the mesh
@@ -19,10 +19,8 @@ function Shape(props: ShapeProps) {
   // "The exclamation mark is a non-null assertion that will let TS know that ref.current is defined when we access it in effects."
   const meshRef = useRef<Mesh>(null!);
 
-  // States
+  // States and Refs
   const [active, setActive] = useState(false);
-  // todo: ask if useRef makes sense in this context or if there is a better way
-  // todo: (this needs to be accessible in handleClick callback and in useFrame and should not trigger a re-render)
   const rotating = useRef(true);
 
   // React Spring properties (Imperative API)
@@ -52,7 +50,7 @@ function Shape(props: ShapeProps) {
       api.start({
         scale: expanded ? 1.3 : 1,
       });
-    }
+    };
   }, []);
   const handlePointerOver = () => {
     // starts hovering
@@ -77,32 +75,35 @@ function Shape(props: ShapeProps) {
   function cloneShape(type: string) {
     if (!active) {
       setActive(true);
-      props.setModules([...props.modules, new Module(props.type, meshRef.current.position)]);
+      props.setModules([
+        ...props.modules,
+        new Module(props.type, meshRef.current.position),
+      ]);
     }
   }
 
   // JSX
   return (
     <>
-    <DragControls
-      onDragStart={() => {
-        cloneShape(props.type);
-      }}
-    >
-      <animated.mesh
-        ref={meshRef}
-        position={props.position}
-        scale={springs.scale}
-        onPointerOver={handlePointerOver}
-        onPointerOut={handlePointerOut}
-        onContextMenu={handleClick()}  // right click
+      <DragControls
+        onDragStart={() => {
+          cloneShape(props.type);
+        }}
       >
-        <boxGeometry args={[1, 1, 1]} />
-        <animated.meshStandardMaterial color={springs.color} />
-        {/* uncomment the following to track whether this object re-renders due to state change */}
-        {/* <Html><p style={{color: 'white'}}>Render ID – {Math.random()}</p></Html> */}
-      </animated.mesh>
-    </DragControls>
+        <animated.mesh
+          ref={meshRef}
+          position={props.position}
+          scale={springs.scale}
+          onPointerOver={handlePointerOver}
+          onPointerOut={handlePointerOut}
+          onContextMenu={handleClick()} // right click
+        >
+          <boxGeometry args={[1, 1, 1]} />
+          <animated.meshStandardMaterial color={springs.color} />
+          {/* uncomment the following to track whether this object re-renders due to state change */}
+          {/* <Html><p style={{color: 'white'}}>Render ID – {Math.random()}</p></Html> */}
+        </animated.mesh>
+      </DragControls>
     </>
   );
 }
